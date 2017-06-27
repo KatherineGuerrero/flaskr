2dc78f2fd50f22bcab9863fc4409738a3ec95801
entamburini <entamburini@uc.cl>
2017-06-27 19:03:09 -0400

    t5.4

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -72,6 +72,14 @@ def api_post():
             return '404 not found D:'
 
 
+@app.after_request  
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
 @app.route("/postgres")
 def postgres():
     query = request.args.get("query")
