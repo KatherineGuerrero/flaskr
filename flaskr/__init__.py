4856cc6302f6d36b2963cc601170d783731e3624
entamburini <entamburini@uc.cl>
2017-06-28 18:23:12 -0400

    cors

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,7 @@ from bson import json_util
 from pymongo import MongoClient
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, current_app, make_response, jsonify
+from flask_cors import CORS
 from datetime import timedelta
 from functools import update_wrapper
 
@@ -17,6 +18,8 @@ def create_app():
     return app
 
 app = create_app()
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 # REPLACE WITH YOUR DATABASE NAME
 MONGODATABASE = "genuino"
@@ -38,48 +41,6 @@ postgresdb = psycopg2.connect(
 
 # Cambiar por Path Absoluto en el servidor
 QUERIES_FILENAME = '/var/www/flaskr/queries'
-
-
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, str):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, str):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
 
 
 @app.route("/")
@@ -104,15 +65,13 @@ def mongo():
         return "ok"
 
 
-@app.route("/date", methods=['GET', 'POST'])
-@crossdomain(origin='*', methods=['GET', 'POST'])
+@app.route("/api/c1", methods=['GET', 'POST'])
 def api_post():
     if request.method == 'GET':
-        response = jsonify({'some': 'data'})
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        prueba = jsonify({'date': '11-11-1111'})
+        return 'Works'
     elif request.method == 'POST':
-        return 'lala'
+        return 'Works'
 
 
 @app.route("/postgres")
