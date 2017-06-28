1e81761587586802dd04ef2b96c72cac0c49a5e7
entamburini <entamburini@uc.cl>
2017-06-28 19:13:22 -0400

    cors1.3

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -65,10 +65,10 @@ def mongo():
         return "ok"
 
 
-@app.route("/cosa/<string:fecha>")
-@cross_origin(origin='*')
-def api_post(fecha):
-    return fecha
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 
 @app.route("/postgres")
