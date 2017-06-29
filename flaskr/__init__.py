f816ab18306f65e745726413d75846b61ade04b2
entamburini <entamburini@uc.cl>
2017-06-29 08:27:25 -0400

    mongotest1.22

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -81,7 +81,7 @@ def api(consulta, datos1):
         separados = datos1.split('_')
         numero = separados[0]
         limite = separados[1]
-        consultilla = escuchas.find({'numero': numero}).sort({'fecha': -1})
+        consultilla = escuchas.find({'numero': numero}).sort([['fecha', DESCENDING]])
         return json_util.dumps(consultilla, sort_keys=True, indent=4)
 
 
