5d9ee5c95a402093e2a0ebe64e99da12e860099b
JAVIERA JORQUERA <jtjorquera@uc.cl>
2017-06-28 22:28:52 -0400

    try

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -64,9 +64,10 @@ def mongo():
     else:
         return "ok"
 
+#@app.route("/post/<int:post_id>')
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
+@app.route("/post")
+def show_post():
     # show the post with the given id, the id is an integer
     return render_template('file.html')
 
